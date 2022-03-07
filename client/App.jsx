864d620aefe42c7003e100539fffc503a113eb10
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-07 13:24:56 -0500

    pkg.json and babel

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+const URL =
+  'https://fsa-puppy-bowl.herokuapp.com/api/2202-FTB-ET-WEB-FT/players'
 
 const App = () => {
-  return <div>Hello There</div>
+  async function getThemPuppies() {
+    const stuff = await fetch(URL)
+    console.log(stuff)
+  }
+
+  useEffect(() => {
+    async function getEm() {
+      await getThemPuppies()
+    }
+    getEm()
+  }, [])
+  return <div>Hello</div>
 }
 
 export default App
