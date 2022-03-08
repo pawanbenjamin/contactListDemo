b5633cec16dd601fff84f974aefaf6bb06004e20
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-07 20:33:09 -0500

    added dummy data

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,12 @@ import { useState } from 'react'
 const URL =
   'https://fsa-puppy-bowl.herokuapp.com/api/2202-FTB-ET-WEB-FT/players'
 
+const dummyDoggos = [
+  { name: 'Ricky', breed: 'smol' },
+  { name: 'Jerry', breed: 'growly' },
+  { name: 'Stanley', breed: 'great-dane' },
+]
+
 const App = () => {
   return <div>Hello</div>
 }
